daddf27a1612883f72abfd9d77a25a7bf896ecd2
flrent <florent@smumgmug.com>
2023-06-27 22:11:59 -0700

    Better version

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://florentlamoureux.fr',
   integrations: [mdx(), sitemap(), tailwind(), react()]
 });
