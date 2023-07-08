afc09b1f5728dbe27594b4b49fbd18416fd8bf33
flrent <flrent@me.com>
2023-07-07 22:05:21 -0700

    Add dark mode to blog

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,9 +3,13 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	plugins: [],
+	darkMode: "class",
+	colors: {
+		orange: '#e45b00'
+	},
 	theme: {
 		extend: {
 		  screens: { 'sm': { 'max': '640px' } },
 		},
-	  },
+	}
 }
