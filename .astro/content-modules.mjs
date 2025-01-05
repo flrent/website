69e9ac56481339d61baf2323d21a3328965f7b12
flrent <flrent@me.com>
2025-01-04 21:50:01 -0800

    Upgrade astro and add 404

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,5 @@
+
+export default new Map([
+["src/content/blog/ai-desktop-private-llm.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fai-desktop-private-llm.mdx&astroContentModuleFlag=true")],
+["src/content/blog/twitter-threads-chrome-extension.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Ftwitter-threads-chrome-extension.mdx&astroContentModuleFlag=true")]]);
+		
