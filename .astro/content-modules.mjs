1e444d86876efa5bd658bd5dc74e89685f0b45d8
flrent <flrent@me.com>
2025-01-31 16:56:46 -0800

    Add surfers of the bay area photo blog

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,6 +1,7 @@
 
 export default new Map([
-["src/content/blog/ai-desktop-private-llm.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fai-desktop-private-llm.mdx&astroContentModuleFlag=true")],
+["src/content/flotogramme/photo-japan-ricoh.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fflotogramme%2Fphoto-japan-ricoh.mdx&astroContentModuleFlag=true")],
+["src/content/flotogramme/surfers-sanfrancisco-bay-area-photos.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fflotogramme%2Fsurfers-sanfrancisco-bay-area-photos.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/twitter-threads-chrome-extension.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Ftwitter-threads-chrome-extension.mdx&astroContentModuleFlag=true")],
-["src/content/flotogramme/photo-japan-ricoh.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fflotogramme%2Fphoto-japan-ricoh.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/ai-desktop-private-llm.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fai-desktop-private-llm.mdx&astroContentModuleFlag=true")]]);
 		
